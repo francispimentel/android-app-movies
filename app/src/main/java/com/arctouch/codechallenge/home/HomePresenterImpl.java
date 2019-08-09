b8571fc0c6f3e223aaf32031a49a72aab8ada775
Francis Pimentel <franciserik92@gmail.com>
2019-08-09 09:00:41 -0300

    Small refactor in HomePresenterImpl to improve readability

--- app/src/main/java/com/arctouch/codechallenge/home/HomePresenterImpl.java
+++ app/src/main/java/com/arctouch/codechallenge/home/HomePresenterImpl.java
@@ -4,6 +4,7 @@ import com.arctouch.codechallenge.api.TmdbApiClient;
 import com.arctouch.codechallenge.data.Cache;
 import com.arctouch.codechallenge.model.Genre;
 import com.arctouch.codechallenge.model.Movie;
+import com.arctouch.codechallenge.model.MoviesResponse;
 
 import java.util.ArrayList;
 
@@ -95,47 +96,33 @@ public class HomePresenterImpl implements HomePresenter {
         Disposable disposable = TmdbApiClient.getApi().upcomingMovies(TmdbApiClient.API_KEY, TmdbApiClient.DEFAULT_LANGUAGE, page, TmdbApiClient.DEFAULT_REGION)
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(response -> {
-                    for (Movie movie : response.results) {
-                        movie.genres = new ArrayList<>();
-                        for (Genre genre : Cache.getGenres()) {
-                            if (movie.genreIds.contains(genre.id)) {
-                                movie.genres.add(genre);
-                            }
-                        }
-                    }
-                    Cache.addMovies(page, response.results);
-                    Cache.setTotalPages(response.totalPages);
-                    if (page == 1L) {
-                        view.setMoviesList(response.results);
-                    } else {
-                        view.appendMoviesList(response.results);
-                    }
-                });
+                .subscribe(response -> onMovieResponse(page, response));
         compositeDisposable.add(disposable);
     }
-
+    
     private void performSearch(Long page) {
         Disposable disposable = TmdbApiClient.getApi().searchMovies(TmdbApiClient.API_KEY, searchTerm, page, TmdbApiClient.DEFAULT_LANGUAGE, TmdbApiClient.DEFAULT_REGION)
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(response -> {
-                    for (Movie movie : response.results) {
-                        movie.genres = new ArrayList<>();
-                        for (Genre genre : Cache.getGenres()) {
-                            if (movie.genreIds.contains(genre.id)) {
-                                movie.genres.add(genre);
-                            }
-                        }
-                    }
-                    Cache.addMovies(page, response.results);
-                    Cache.setTotalPages(response.totalPages);
-                    if (page == 1L) {
-                        view.setMoviesList(response.results);
-                    } else {
-                        view.appendMoviesList(response.results);
-                    }
-                });
+                .subscribe(response -> onMovieResponse(page, response));
         compositeDisposable.add(disposable);
+    }
+
+    private void onMovieResponse(Long page, MoviesResponse response) {
+        for (Movie movie : response.results) {
+            movie.genres = new ArrayList<>();
+            for (Genre genre : Cache.getGenres()) {
+                if (movie.genreIds.contains(genre.id)) {
+                    movie.genres.add(genre);
+                }
+            }
+        }
+        Cache.addMovies(page, response.results);
+        Cache.setTotalPages(response.totalPages);
+        if (page == 1L) {
+            view.setMoviesList(response.results);
+        } else {
+            view.appendMoviesList(response.results);
+        }
     }
 }
